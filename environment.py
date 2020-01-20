3290d3bed5ff3ba410625f2adb38aef4575ff135
Rui Frazão <ruifilipefrazao@gmail.com>
2020-01-20 21:55:10 +0000

    environment variables and first version of openpose extraction

--- environment.py
+++ environment.py
@@ -7,15 +7,48 @@ VALID_VIDEO_TYPES = ['.avi', '.wmv']
 # PATHS
 TESE_HOME = os.path.dirname(os.path.abspath(__file__))
 
-## OpenFace
+# OpenFace
+# FaceLandmarkImg executable is for individual image analysis (can either contain one or more faces)
+# FaceLandmarkVidMulti is intended for sequence analysis that contain multiple faces
+# FeatureExtraction executable is used for sequence analysis that contain a single face
 OPENFACE_HOME = TESE_HOME + "/packages/openface"
 OPENFACE_BUILD = OPENFACE_HOME + "/build/bin/"
 OPENFACE_FACE_LANDMARK_IMG = OPENFACE_BUILD + "FaceLandmarkImg"
 OPENFACE_FACE_LANDMARK_VID = OPENFACE_BUILD + "FaceLandmarkVid"
 OPENFACE_FACE_LANDMARK_VID_MULTI = OPENFACE_BUILD + "FaceLandmarkVidMulti"
 OPENFACE_FEATURE_EXTRACTION = OPENFACE_BUILD + "FeatureExtraction"
-OPENFACE_OUT = TESE_HOME + "/packages/openface_output"
+OPENFACE_OUTPUT_DIR = "output/openface_output"
 
-## OpenPose
+# OpenFace Output Commands:
+# -au_static    : static models only rely on a single image to make an estimate of AU
+#                   presence or intensity, while dynamic ones calibrate to a person by performing person
+#                   normalization in the video, they also attempt to correct for over and under prediction of AUs.
+#                   By default OpenFace uses static models on images and dynamic models on image sequences and videos.
+#                   DOC Ref: https://github.com/TadasBaltrusaitis/OpenFace/wiki/Action-Units#static-vs-dynamic
+# -2Dfp         : output 2D landmarks in pixels
+# -3Dfp         : output 3D landmarks in milimeters
+# -pdmparams    : output rigid and non-rigid shape parameters:
+#                   * Rigid shape parameters describe the placement of the face in the image
+#                       (scaling, rotation, and translation);
+#                   * Non-rigid shape parameters on the other hand describe the deformation
+#                       of the face due to identity or expression (wider or taller faces, smiles, blinks etc.)
+# -pose         : output head pose (location and rotation)
+# -aus          : output the Facial Action Units
+# -gaze         : output gaze and related features (2D and 3D locations of eye landmarks)
+# -hogalign     : output extracted HOG feaure file
+# -simalign     : output similarity aligned images of the tracked faces
+# -nobadaligned : if outputting similarity aligned images, do not output from frames where detection failed
+#                   or is unreliable (thus saving some disk space)
+# -tracked      : output video with detected landmarks
+
+
+OPENFACE_OUTPUT_COMMANDS = ['-3Dfp', '-pdmparams',
+                            '-pose', '-aus', '-gaze']
+
+# OpenPose
 OPENPOSE_HOME = TESE_HOME + "/packages/openpose"
-OPENPOSE_OUT = TESE_HOME + "/packages/openpose_output"
+OPENPOSE_OUT = "output/openpose_output"
+
+# OpenCV
+OPENCV_HOME = TESE_HOME + "/packages/opencv"
+OPENCV_OUT = "output/opencv_output"
--- openface_extract.py
+++ openface_extract.py
@@ -7,13 +7,15 @@ from utils import print_assertion_error
 
 MEDIA_TYPE_IMAGE = 'img'
 MEDIA_TYPE_VIDEO = 'vid'
+MEDIA_TYPE_FEATURES = ''
+MEDIA_TYPE_CAM = 'cam'
 
 # class OpenFaceExtractor:
 
 parser = argparse.ArgumentParser(
     description='Process Facial Data using OpenFace')
 parser.add_argument('media_type', type=str, choices=[
-                    'img', 'vid'], help='Media type')
+                    MEDIA_TYPE_IMAGE, MEDIA_TYPE_VIDEO, MEDIA_TYPE_CAM], help='Media type')
 parser.add_argument('media_files', type=str, nargs='*', help='Media files')
 parser.add_argument('-dir', '--directory', action="store_true",
                     help='Media files path')
@@ -25,36 +27,65 @@ parser.add_argument('-v', '--verbose', help='Whether or not responses should be 
 args = vars(parser.parse_args())
 
 
-def openface_landmark_img(img_files: list, verbose: bool = False):
+def openface_img(img_files: list, verbose: bool = False):
+    '''
+    Process single image input. Only supports single face processing.
+    '''
     assert isinstance(img_files, list), print_assertion_error(
         "img_files", "list")
     cmd_list = [env.OPENFACE_FACE_LANDMARK_IMG]
     for file_path in img_files:
         cmd_list += ['-f', file_path]
 
+    cmd_list += ['-out_dir', env.OPENFACE_OUTPUT_DIR]
     print(cmd_list)
     subprocess.call(cmd_list)
 
 
-def openface_landmark_vid(vid_files: list, multi: bool = False, verbose: bool = False):
+def openface_vid(vid_files: list, multi: bool = False, verbose: bool = False):
+    '''
+    Process video input with single of multiple faces.
+
+    In this case, OPENFACE_FEATURE_EXTRACTION will be used instead of OPENFACE_FACE_LANDMARK_VID
+    as they both serve the same purpose of single face tracking, but only OPENFACE_FEATURE_EXTRACTION
+    processed the images regarding the frame sequence, while OPENFACE_FACE_LANDMARK_VID processes
+    each frame as it was a random image. (Unsure what is the difference, but such inference is
+    described in documentation - https://github.com/TadasBaltrusaitis/OpenFace/wiki/Command-line-arguments#example-uses)
+    '''
     assert isinstance(vid_files, list), print_assertion_error(
         "vid_files", "list")
     assert isinstance(multi, bool), print_assertion_error(
         "multi", "bool")
 
     if not multi:
-        cmd_list = [env.OPENFACE_FACE_LANDMARK_VID]
+        cmd_list = [env.OPENFACE_FEATURE_EXTRACTION]
     else:
         cmd_list = [env.OPENFACE_FACE_LANDMARK_VID_MULTI]
 
     for file_path in vid_files:
         cmd_list += ['-f', file_path]
 
+    cmd_list += env.OPENFACE_OUTPUT_COMMANDS
+    cmd_list += ['-out_dir', env.OPENFACE_OUTPUT_DIR]
     print(cmd_list)
-    # subprocess.call(cmd_list)
+    subprocess.call(cmd_list)
 
+
+def openface_cam(device: list = 0, verbose: bool = False):
+    '''
+    Process data directly from device (webcam = 0) input
+    '''
+    assert isinstance(device, int), print_assertion_error(
+        "device", "int")
+
+    cmd_list = [env.OPENFACE_FEATURE_EXTRACTION]
+    cmd_list += ['-device', '0']
+    cmd_list += ['-out_dir', env.OPENFACE_OUTPUT_DIR]
+    print(cmd_list)
+    subprocess.call(cmd_list)
 
 # print(args)
+
 
 media_type = args['media_type']
 valid_types = (env.VALID_IMAGE_TYPES if media_type ==
@@ -64,7 +95,7 @@ directory = args['directory']
 multi = args['multi']
 verbose = args['verbose']
 
-if not media_files:
+if not media_files and media_type != MEDIA_TYPE_CAM:
     print("Error: No media files passed")
     exit()
 
@@ -78,6 +109,8 @@ for file in media_files:
         exit()
 
 if media_type == MEDIA_TYPE_IMAGE:
-    openface_landmark_img(media_files)
+    openface_img(media_files)
 elif media_type == MEDIA_TYPE_VIDEO:
-    openface_landmark_vid(media_files, multi=multi)
+    openface_vid(media_files, multi=multi)
+elif media_type == MEDIA_TYPE_CAM:
+    openface_cam(device=0)
