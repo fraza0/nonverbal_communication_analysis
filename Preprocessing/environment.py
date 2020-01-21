7477b5df640a39a2bd962500634f266bea0f28a2
Rui Frazão <ruifilipefrazao@gmail.com>
2020-01-21 21:26:08 +0000

    preprocessing reorganization

---
+++ Preprocessing/environment.py
@@ -0,0 +1,15 @@
+import os
+
+TESE_HOME = os.path.dirname(os.path.abspath(__file__))
+
+# VARIABLES
+VALID_IMAGE_TYPES = ['.jpg', '.jpeg', '.png']
+VALID_VIDEO_TYPES = ['.avi', '.wmv', '.mp4']
+
+# PATHS
+DATASET_DIR = TESE_HOME + "/DATASET_DEP"
+DATASET_PC1 = DATASET_DIR + "/" + "Videos_LAB_PC1"
+DATASET_PC2 = DATASET_DIR + "/" + "Videos_LAB_PC2"
+DATASET_PC3 = DATASET_DIR + "/" + "Videos_LAB_PC3"
+
+DATASET_SYNC = DATASET_DIR + "/" + "SYNC"
