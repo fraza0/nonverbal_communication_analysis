2f25fb407c21c23a328be48168cc82d4b3088ea7
Rui Frazão <ruifilipefrazao@gmail.com>
2020-01-30 15:43:04 +0000

    video synchronization with cut option

--- Preprocessing/video_synchronization.py
+++ Preprocessing/video_synchronization.py
@@ -1,8 +1,14 @@
 from os.path import isfile, splitext
+from datetime import datetime
 import argparse
 import cv2
+import os
+import errno
+import cv2
+import os
+import errno
 
-from environment import VALID_VIDEO_TYPES, VALID_TIMESTAMP_FILES, TIMESTAMP_THRESHOLD, DATASET_SYNC, FOURCC
+from environment import VALID_VIDEO_TYPES, VALID_TIMESTAMP_FILES, TIMESTAMP_THRESHOLD, DATASET_SYNC, FOURCC, FRAME_SKIP
 
 # python Preprocessing/video_synchronization.py -f DATASET_DEP/Videos_LAB_PC1/Videopc118102019021136.avi -t DATASET_DEP/Videos_LAB_PC1/Timestamppc118102019021136.txt -f DATASET_DEP/Videos_LAB_PC2/Videopc218102019021117.avi -t DATASET_DEP/Videos_LAB_PC2/Timestamppc218102019021117.txt -f DATASET_DEP/Videos_LAB_PC3/Videopc318102019021104.avi -t DATASET_DEP/Videos_LAB_PC3/Timestamppc318102019021104.txt
 
@@ -10,14 +16,19 @@ from environment import VALID_VIDEO_TYPES, VALID_TIMESTAMP_FILES, TIMESTAMP_THRE
 class CameraVideo:
     '''
     Camera Video Object: Unit to Synchronize
+
+    TODO: EXPLAIN EACH ATTRIBUTE OF CAMERAVIDEO OBJECT
+
     '''
     current_timestamp = 0
 
-    def __init__(self, title: str, video_path: str, timestamp_path: str, output_path: str):
+    def __init__(self, title: str, video_path: str, timestamp_path: str, output_path: str, write: bool = True):
         self.title = title
         self.cap = cv2.VideoCapture(video_path)
         self.timestamps = open(timestamp_path, 'r')
+        self.current_frame_idx = 0
         self.current_timestamp = 0
+        self.markers = dict()
         self.ret = None
         self.frame = None
         frame_width = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -34,8 +45,10 @@ def timestamp_align(cap_list: list):
     '''
     Determine which video(s) need to be aligned
 
-    Returns: True if they are aligned -- Second tuple element is needed to be able to unpack when videos are synced
-             List of videos that need to be aligned
+    Returns Tuple:
+        * (True, None) - If they are aligned. Ignore first iteration when frame and timestamp is read.
+        * (True, True) - If they are aligned.
+        * (to_align: list, align_by: CameraVideo) - List of videos that need to be aligned, CameraVideo object of reference video
     '''
     align_by = max(cap_list, key=lambda x: x.current_timestamp)
     is_synced = all(align_by.current_timestamp - vid.current_timestamp <=
@@ -53,9 +66,19 @@ def timestamp_align(cap_list: list):
         if align_ts - vid.current_timestamp > TIMESTAMP_THRESHOLD:
             to_align.append(vid)
 
-    # print([vid.current_timestamp for vid in cap_list])
-    # print([vid.title for vid in to_align])
     return to_align, align_by
+
+
+def cut_from_until(vid, _from: int, _until: int):
+    '''
+    Write video from frame f_init until f_end
+    '''
+
+    vid.cap.set(cv2.CAP_PROP_POS_FRAMES, _from)
+
+    for _ in range(_from, _until):
+        vid.ret, vid.frame = vid.cap.read()
+        vid.writer.write(vid.frame)
 
 
 parser = argparse.ArgumentParser(
@@ -66,82 +89,136 @@ parser.add_argument('-t' '--timestamp', type=str, nargs=1,
                     dest='timestamp_files', action='append', help='Media files')
 parser.add_argument('-v', '--verbose', help='Whether or not responses should be printed',
                     action='store_true')
-# TODO: Add read all files directly from directory. Timestamp files must have same file_name as video file name
-# parser.add_argument('-dir', '--directory', action="store_true", help='Media files path')
+parser.add_argument('-w', '--write', help="Where or not to output synchronized videos",
+                    action='store_true')
 args = vars(parser.parse_args())
 
 video_files = [vf[0] for vf in args['video_files']]
 timestamp_files = [tf[0] for tf in args['timestamp_files']] if args['timestamp_files'] is not None else [
     splitext(f[0])[::-1].replace('Video'[::-1], 'Timestamp'[::-1])[::-1] + ".txt" for f in video_files]
+write = args['write']
 verbose = args['verbose']
 
-for file in video_files:
-    _, file_extension = splitext(file)
-    if file_extension not in VALID_VIDEO_TYPES:
-        print("Not supported or invalid video file type (%s). File must be {%s}" % (
-            file, VALID_VIDEO_TYPES))
+
+if __name__ == "__main__":
+    for file in video_files:
+        _, file_extension = splitext(file)
+        if file_extension not in VALID_VIDEO_TYPES:
+            print("Not supported or invalid video file type (%s). File must be {%s}" % (
+                file, VALID_VIDEO_TYPES))
+            exit()
+
+    for file in timestamp_files:
+        _, file_extension = splitext(file)
+        if file_extension not in VALID_TIMESTAMP_FILES:
+            print("Not supported or invalid timestamp file type (%s). Check input files" %
+                  VALID_TIMESTAMP_FILES)
+            exit()
+
+    if len(video_files) != 3 and len(timestamp_files) != 3:
+        print("Only 3 video files and corresponding timestamps available")
         exit()
 
-for file in timestamp_files:
-    _, file_extension = splitext(file)
-    if file_extension not in VALID_TIMESTAMP_FILES:
-        print("Not supported or invalid timestamp file type (%s). Check input files" %
-              VALID_TIMESTAMP_FILES)
+    cap_list = list()
+    out_dir = DATASET_SYNC+"/%s/" % video_files[0][-18:-8]
+
+    try:
+        os.makedirs(out_dir)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+
+    for i in range(len(video_files)):
+        _id = str(i+1)
+        vid = CameraVideo("VID"+_id, video_files[i], timestamp_files[i], out_dir +
+                          "sync_vid"+_id+"_"+splitext(video_files[i])[0].split('/')[-1]+".avi", write)
+        cap_list.append(vid)
+
+    if not all(vid.cap.isOpened() for vid in cap_list):
+        print("Error opening video stream or file")
         exit()
 
+    frame_count = 0
+    marker_validator = {ord('1'): False, ord('2'): False,
+                        ord('3'): False, ord('4'): False}
 
-if len(video_files) != 3 and len(timestamp_files) != 3:
-    print("Only 3 video files and corresponding timestamps available")
-    exit()
+    INIT_TIME = datetime.now()
 
-vid1 = CameraVideo("VID1", video_files[0], timestamp_files[0],
-                   DATASET_SYNC+"/out_vid1_"+splitext(video_files[0])[0].split('/')[-1]+".avi")
-vid2 = CameraVideo("VID2", video_files[1], timestamp_files[1],
-                   DATASET_SYNC+"/out_vid2_"+splitext(video_files[1])[0].split('/')[-1]+".avi")
-vid3 = CameraVideo("VID3", video_files[2], timestamp_files[2],
-                   DATASET_SYNC+"/out_vid3_"+splitext(video_files[2])[0].split('/')[-1]+".avi")
+    while(all(vid.cap.isOpened() for vid in cap_list)):
 
-cap_list = [vid1, vid2, vid3]
+        alignment, align_by = timestamp_align(cap_list)
+        to_align_list = cap_list if alignment is True else alignment
 
-if not all(vid.cap.isOpened() for vid in cap_list):
-    print("Error opening video stream or file")
-    exit()
+        for vid in to_align_list:
+            # Read frame
+            vid.ret, vid.frame = vid.cap.read()
+            vid.current_frame_idx += 1
+            # Update current_timestamp
+            file_ts = vid.timestamps.readline()
+            vid.current_timestamp = int(file_ts) if file_ts is not '' else -1
 
+        key = cv2.waitKey(25) & 0xff
 
-while((vid1.cap.isOpened() and vid2.cap.isOpened() and vid3.cap.isOpened())):
+        if key == 0x20:                             # Pause Video
+            while cv2.waitKey(-1) & 0xFF != 0x20:   # Resume Video
+                pass
 
-    alignment, align_by = timestamp_align(cap_list)
-    to_align_list = cap_list if alignment is True else alignment
+        if key == ord('d'):                         # Skip
+            for vid in cap_list:
+                vid.current_frame_idx += FRAME_SKIP
+                vid.cap.set(cv2.CAP_PROP_POS_FRAMES, vid.current_frame_idx)
+                # vid.timestamps.seek(vid.timestamps.tell())
 
-    for vid in to_align_list:
-        # Read frame
-        vid.ret, vid.frame = vid.cap.read()
-        # Update current_timestamp
-        vid.current_timestamp = int(vid.timestamps.readline())
+        if key == ord('a'):                         # Jump Back
+            for vid in cap_list:
+                vid.current_frame_idx -= FRAME_SKIP
+                vid.cap.set(cv2.CAP_PROP_POS_FRAMES, vid.current_frame_idx)
+                # vid.timestamps.seek(vid.timestamps.tell())
 
-    key = cv2.waitKey(25) & 0xff
+        if key >= ord('1') and key <= ord('4'):
+            idx = key  # 49,50,51,52
+            marker_validator[idx] = True
+            for vid in cap_list:
+                vid.markers[idx] = vid.current_frame_idx
 
-    if key == ord('p'):                             # Pause Video
-        while cv2.waitKey(-1) & 0xFF != ord('p'):   # Resume Video
-            pass
+        if all([vid.ret for vid in cap_list]):
+            for vid in cap_list:
+                if alignment == True and align_by is not None:  # Videos are in sync
+                    cv2.imshow(vid.title, vid.frame)
 
-    if all([vid.ret for vid in cap_list]):
+            if key == ord('s'):                 # Save
+                if write:
+                    if all(flag is True for flag in marker_validator.values()):
+                        if verbose:
+                            print("Start writting phase")
 
-        for vid in cap_list:
+                        for vid in cap_list:
+                            first_experience_markers = list(
+                                vid.markers.keys())[:2]
+                            second_experience_markers = list(
+                                vid.markers.keys())[2:]
 
-            if alignment == True and align_by is not None:
-                # print("WRITE BECAUSE THEY ARE SYNCED")
-                vid.writer.write(vid.frame)
-                cv2.imshow(vid.title, vid.frame)
+                            if verbose:
+                                print("Vid%s Saving Task 1" % vid.title)
+                            cut_from_until(
+                                vid, vid.markers[first_experience_markers[0]], vid.markers[first_experience_markers[1]])
 
-        if key == ord('q'):
+                            if verbose:
+                                print("Vid%s Saving Task 2" % vid.title)
+                            cut_from_until(
+                                vid, vid.markers[second_experience_markers[0]], vid.markers[second_experience_markers[1]])
+
+                break
+
+            if key == ord('q'):
+                break
+
+        else:
             break
 
-    else:
-        break
+    for vid in cap_list:
+        vid.cap.release()
+        if write:
+            vid.writer.release()
 
-for vid in cap_list:
-    vid.cap.release()
-    vid.writer.release()
-
-cv2.destroyAllWindows()
+    cv2.destroyAllWindows()
